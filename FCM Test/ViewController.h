3dcb8cc179d342d2673428a8b03a21fe92b25761
IvanEscamilla <Naruxhinal2054192ws>
2016-08-09 17:11:20 -0500

    Initial Commit

---
+++ FCM Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FCM Test
+//
+//  Created by Luis Ivan Escamilla Estrada on 8/9/16.
+//  Copyright © 2016 liee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FCM Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FCM Test
+//
+//  Created by Luis Ivan Escamilla Estrada on 8/9/16.
+//  Copyright © 2016 liee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
